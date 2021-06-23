a64ae8ad4f1a948e30ce2abfc5df00df3c81dad3
syrohei <syrohei@gmail.com>
2021-06-23 18:02:10 +0900

    feat: added custom gas limit

--- scripts/init.js
+++ scripts/init.js
@@ -5,9 +5,9 @@ module.exports = async function (done) {
     //const swingby = "0xfcd51b56e65605c33024a9e98a7aadff2e1a15b9" // mStable on goerli testnet
     const swingby = "0x8287c7b963b405b7b8d467db9d79eec40625b13a" // swingby on mainnet
     const swingbyPerBlock = new BN(4).mul(new BN(10).pow(new BN(18)))
-    const startBlock = 4983920
+    const startBlock = 12688564
     const bonusEndBlock = startBlock
-    const tx = await cl.init(swingby, swingbyPerBlock, startBlock, bonusEndBlock)
+    const tx = await cl.init(swingby, swingbyPerBlock, startBlock, bonusEndBlock, { gas: 128000 })
     console.log(tx.tx)
     done()
 };
