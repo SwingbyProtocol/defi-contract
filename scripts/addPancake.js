1fc944a33d1750af691006b5dafc7af21b445fac
syrohei <syrohei@gmail.com>
2021-07-12 14:51:38 +0800

    R4R: pre release for bsc testnet

--- scripts/addPancake.js
+++ scripts/addPancake.js
@@ -4,7 +4,10 @@ module.exports = async function (done) {
     cl = await ChefLink.deployed()
     const LPT = "0xa88921dc290f888b5ee574cf2cd1599f412f1534" // LPT test on bsc_testnet
     const allocPoint = 10000
-    const tx = await cl.add(allocPoint, LPT, true, { gas: 150000 })
+    const farmCoin = "0x22dc6cf365476465ca3897dc5a23e7cb0a65a482" // cakes on bsc_testnet 
+    const farmContract = "0x22f6de9b90783fa94afb08d34fcfda0ceb07988b"
+    const ppid = 1
+    const tx = await cl.add(allocPoint, LPT, farmCoin, farmContract, ppid, true, { gas: 150000 })
     console.log(tx.tx)
     done()
 };
--- scripts/init_bsc.js
+++ scripts/init_bsc.js
@@ -5,12 +5,9 @@ module.exports = async function (done) {
     const swingby = "0x1d606092b6c14d940bc421884009af9ea6f54c1c" // swingby on bsc_testnet
     // const swingby = "0x8287c7b963b405b7b8d467db9d79eec40625b13a" // swingby on mainnet
     const swingbyPerBlock = new BN(4).mul(new BN(10).pow(new BN(18)))
-    const startBlock = 10415559
+    const startBlock = 10520535
     const bonusEndBlock = startBlock
-    const farmCoin = "0x22dc6cf365476465ca3897dc5a23e7cb0a65a482" // cakes on bsc_testnet 
-    const farmContract = "0x22f6de9b90783fa94afb08d34fcfda0ceb07988b"
-    const ppid = 1
-    const tx = await cl.init(swingby, swingbyPerBlock, startBlock, bonusEndBlock, farmCoin, farmContract, ppid, { gas: 208000 })
+    const tx = await cl.init(swingby, swingbyPerBlock, startBlock, bonusEndBlock, { gas: 208000 })
     console.log(tx.tx)
     done()
 };
