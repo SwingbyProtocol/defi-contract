7f25678885bd5a673fb4b0514beb05af6fb0ce7e
syrohei <syrohei@gmail.com>
2021-08-07 01:16:22 +0800

    feat: added draft

---
+++ contracts/interfaces/ISwapContractMin.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity 0.8.0;
+
+interface ISwapContractMin {
+    function getCurrentPriceLP() external view returns (uint256);
+
+    function getDepositFeeRate(address _token, uint256 _amountOfFloat)
+        external
+        view
+        returns (uint256);
+
+    function getFloatReserve(address _tokenA, address _tokenB)
+        external
+        returns (uint256 reserveA, uint256 reserveB);
+}
